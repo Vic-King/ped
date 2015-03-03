909e43fbaa07e26b3fd9cb9610d9c872a7930488
Victor <victorfrancois@free.fr>
2015-03-03 09:33:25 +0100

    maj

--- ped_isv/Point.h
+++ ped_isv/Point.h
@@ -4,13 +4,11 @@
 class Point
 {
 public:
-    Point(float x, float y, float z, float nx, float ny, float nz) : m_X(x), m_Y(y), m_Z(z),
-                                                                     m_NX(nx), m_NY(ny), m_NZ(nz)
+    Point(glm::vec3 position, glm::vec3 normal) : m_position(position), m_normal(normal);
 
 
 private:
-    float m_X, m_Y, m_Z;
-    float m_NX, m_NY, m_NZ;
+    glm::vec3 m_position, m_normal;
 };
 
 #endif // POINT_H
--- ped_isv/main.cpp
+++ ped_isv/main.cpp
@@ -2,12 +2,61 @@
 
 #include "IDataConversion.h"
 #include "DisplayModel.h"
+#include "Point.h"
+#include "VEFModel.h"
 
 using namespace std;
 
-int main()
-{
+void raytrace(/*glm::vec3 ori, glm::vec3 dir*/);
+bool move(/*position, orientation, direction*/);
+
+struct robot {
+    /*glm::vec3 position(x, y, z);
+    glm::vec3 orientation(x, y, z);
+    glm::vec3 direction(x, y, z);*/
+};
+
+int main() {
+    // CONFIG
+    unsigned int nbRay = 180;
+    struct robot WallE;
+
+    bool stop = false;
+    std::vector<Point> pointCloud;
+
+   Point curPoint;
+
+    // TEST MODE
+    VEFModel obj;
+    obj.load("Objects/couloirPED.obj");
+
+    while (!stop) {
+
+        for (int i = 0; i < nbRay; ++i) {
+
+            // TEST MODE
+            raytrace();
+
+            // NORMAL MODE
+            // get drone data
+
+            WallE.move(/*glm::vec3 accPos, glm::vec3 accOri, glm::vec3 accDir*/);
+        }
+
+        stop = !move(/*position, direction*/);
+    }
 
     return 0;
 }
 
+void raytrace(/*glm::vec3 ori, glm::vec3 dir*/) {
+
+}
+
+bool move(/*glm::vec3 accPos, glm::vec3 accOri, glm::vec3 accDir*/) {
+    /*position += accPos;
+    orientation += accOri;
+    direction += accDir;*/
+
+    return true;
+}
